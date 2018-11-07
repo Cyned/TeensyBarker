9d1fc2f3a9f8bcaedfc19cd9dd9d8b26e64af472
kushkamisha <kushkamisha@yahoo.com>
2018-11-07 21:58:13 +0200

    Now can process whole http://santori.com.ua
    To be added: normal logs
    http://santori.com.ua/menyu/menyu-bara/coctails/ doesn't work, should remake pdf file saver

---
+++ collect/main.py
@@ -0,0 +1,11 @@
+from restaurants import Restaurant
+
+
+def main():
+    url = "http://santori.com.ua/"
+    restaurant = Restaurant(url)
+    restaurant.collect_menu()
+
+
+if __name__ == "__main__":
+    main()
--- collect/restaurants.py
+++ collect/restaurants.py
@@ -4,9 +4,9 @@ import requests
 import os
 
 
-class Restaurant:
-    """Finds restaurant website and parse it to get menu files"""
-    def __init__(self, site_url, url):
+class RestaurantPage:
+    """Looks for menu images or pdf files on the restaurant page"""
+    def __init__(self, site_url, url, used_urls=[]):
         """Init some useful values"""
         self.SITE_URL = site_url
         self.IMAGE_FORMATS = [".pdf", ".jpg", ".jpeg"]
@@ -15,22 +15,13 @@ class Restaurant:
         self.url = url  # current url
         self.bs = None  # BeautifulSoup object
         self.menu_urls = []  # all menu urls from site
+        self.used_urls = used_urls  # list of processed urls
 
     def print_menu_urls(self):
         """Display menu_urls list"""
         print("Menu: ")
         for menu_url in self.menu_urls:
             print(menu_url)
-
-    def collect_menu(self):
-        """Find all menu images on the page and download them"""
-        html = urlopen(self.url)
-        self.bs = BeautifulSoup(html.read(), features="lxml")
-
-        self.get_menu_urls()
-        self.print_menu_urls()
-        self.save_menu_images()
-        self.print_menu_urls()
 
     def is_image_link(self, url):
         """Checks is provided url is an image or pdf"""
@@ -67,7 +58,7 @@ class Restaurant:
             f.write(req.content)
 
     # Get menu links
-    def get_menu_urls(self):
+    def search_menu_urls(self):
         """Get all menu links found on the page"""
         links = self.bs.findAll("a")  # look for all links
 
@@ -82,7 +73,9 @@ class Restaurant:
                 m_url = m_url[1:]
 
             if self.is_menu_link(m_url):
-                self.menu_urls.append(self.SITE_URL + m_url)
+                url = self.SITE_URL + m_url
+                if url not in self.used_urls:
+                    self.menu_urls.append(url)
 
         # Remove duplicates
         self.menu_urls = list(set(self.menu_urls))
@@ -100,13 +93,17 @@ class Restaurant:
                 self.save_file_from_url(m_url, filename)
                 # Remove used links from urls list
                 self.menu_urls.remove(m_url)
-                # ... add used urs to appropriate list
+                # Add processed urls to appropriate list
+                self.used_urls.append(m_url)
 
         # Whole website is pdf file - save it
         if self.is_bs_pdf_file():
+            print("PDF URL:", self.url)
             url = self.url[:-1]  # url without last slash
             filename = url[url.rfind("/")+1:] + ".pdf"
             self.save_file_from_url(self.url, filename)
+            # Add processed urls to appropriate list
+            self.used_urls.append(self.url)
 
     def is_bs_pdf_file(self):
         """Checks is current page object is pdf file"""
@@ -117,15 +114,52 @@ class Restaurant:
             return False
             raise
 
+    def collect_menu(self):
+        """Find all menu images on the page and download them"""
+        print("\n\nProcessing ...", self.url)
+        html = urlopen(self.url)
+        self.bs = BeautifulSoup(html.read(), features="lxml")
 
-def main():
-    site_url = "http://santori.com.ua/"
-    url = site_url
-    # url = "http://santori.com.ua/menyu/menyu-bara/"  # current url
-    # url = "http://santori.com.ua/menyu/menyu-bara/bar/"
-    restaurant = Restaurant(site_url, url)
-    restaurant.collect_menu()
+        self.search_menu_urls()
+        # self.print_menu_urls()
+        self.save_menu_images()
+        # self.print_menu_urls()
+
+    def get_menu_urls(self):
+        """Get menu_urls list"""
+        return self.menu_urls
+
+    def get_used_urls(self):
+        """Get used_urls list"""
+        return self.used_urls
 
 
-if __name__ == "__main__":
-    main()
+class Restaurant:
+    """Looks for menu images or pdf files on the whole restaurant website"""
+    def __init__(self, site_url):
+        """Init restaurant url"""
+        self.site_url = site_url
+
+    def collect_menu(self):
+        """Find all menu images on the restaurant website and download them"""
+        urls = [self.site_url]
+        used = []
+        count = 1
+
+        for url in urls:
+            print("\n\n\n============" + str(count) + "============\n\n")
+            page = RestaurantPage(self.site_url, url, used)
+            page.collect_menu()
+
+            menu_urls = page.get_menu_urls()
+            used = page.get_used_urls()
+
+            print("Menu:", menu_urls, end="\n\n")
+            print("URLs list:", urls, end="\n\n")
+            print("Used:", used, end="\n\n")
+
+            for menu_url in menu_urls:
+                if menu_url not in urls:
+                    urls.append(menu_url)
+
+            count += 1
